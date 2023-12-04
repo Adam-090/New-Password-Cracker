cb043c4061070d25e5c4c49b889c6f888c794293
Adam-090 <adam.hathiyari@gmail.com>
2023-12-04 10:52:02 -0600

    Md5 done (python 6?)

---
+++ Python Password/Md5.py
@@ -0,0 +1,27 @@
+#hash attack
+import os 
+import hashlib
+passwordHash = input("Enter the hash you want to convert ")
+paths = "/home/adam/Documents/Gitshub2/New-Password-Cracker/paslist.txt"
+print(os.getcwd())
+count = 0
+with open(paths, "r") as pList:
+    
+    while count < 1000000:
+        count += 1
+ 
+        # Get next line from file
+        line = pList.readline()
+        lineR = line.rstrip()
+        hashedLine = hashlib.md5(lineR.encode('utf-8')).hexdigest()
+        # if line is empty
+        # end of file is reached
+        print("Trying: " + hashedLine)
+        if hashedLine == passwordHash:
+            print("Sucess the hash " + passwordHash + " equals " + lineR)
+            break
+        if count == 10001:
+            print("we could not decode that hash")
+            break
+        #print("Line{}: {}".format(count, line.strip()))
+#test
