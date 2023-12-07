f2facdc3bded121252d5b7b1d89ff4b86c237569
Adam-090 <adam.hathiyari@gmail.com>
2023-12-07 12:52:11 -0600

    [ython9

--- Python Password/Bcrypt.py
+++ Python Password/Bcrypt.py
@@ -1,11 +1,10 @@
-##Bcrypt
+#Bcrypt
 import bcrypt
 import os 
 passwordHash = (input("Enter the hash you want to convert ")).encode('utf-8')
 paths = "/home/adam/Documents/Gitshub2/New-Password-Cracker/paslist.txt"
 print(os.getcwd())
 count = 0
-
 with open(paths, "r") as pList:
     
     while count < 1000000:
